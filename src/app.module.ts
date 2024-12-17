1a212dd2659e5f09d4e3fa0cf5819931b0295765
mujahidraj <mujahidraj65@gmail.com>
2024-12-17 18:30:35 +0600

    v2

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RegistrationModule } from './registration/registration.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 
 @Module({
   
   controllers: [AppController],
   providers: [AppService],
-  imports: [RegistrationModule],
+  imports: [RegistrationModule, PrismaModule],
 })
 export class AppModule {}
--- src/registration/registration.service.ts
+++ src/registration/registration.service.ts
@@ -1,7 +1,10 @@
 import { Injectable, Post } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class RegistrationService {
+
+    constructor(private prisma: PrismaService){}
     
     signup(){
         return 'Sign up Successful.';
